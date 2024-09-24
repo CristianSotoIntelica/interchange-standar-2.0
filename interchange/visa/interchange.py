7eab5cc47180a09b21e5a04c07a5823289b324c6
Daniel Morales Valdivia <daniel.morales@intelica.com>
2024-09-24 11:53:12 -0500

    Interchange load transaction data

--- interchange/visa/interchange.py
+++ interchange/visa/interchange.py
@@ -15,12 +15,33 @@ def calculate_baseii_interchange(
     target_layer: FileStorage.Layer,
     client_id: str,
     file_id: str,
-    origin_subdir="400-BASEII_CAL_DRAFTS",
+    transactions_subdir="300-BASEII_CLN_DRAFTS",
+    calculated_subdir="400-BASEII_CAL_DRAFTS",
     target_subdir="500-BASEII_ITX_DRAFTS",
 ) -> None:
     """
     Calculate interchange fee fields for BASE II transaction data.
     """
+    log.logger.info(
+        f"Reading clean BASE II Transactions from {client_id} file {file_id}"
+    )
+    transactions = fs.read_parquet(
+        origin_layer,
+        client_id,
+        file_id,
+        subdir=transactions_subdir,
+    )
+    log.logger.info(f"Reading calculated field data from {client_id} file {file_id}")
+    calculated = fs.read_parquet(
+        origin_layer,
+        client_id,
+        file_id,
+        subdir=calculated_subdir,
+    )
+    log.logger.info(
+        f"Merging transactional and calculated data from {client_id} file {file_id}"
+    )
+    data = transactions.join(calculated, how="left", lsuffix="_baseii")
     pass
 
 
@@ -30,3 +51,8 @@ def calculate_sms_interchange() -> None:
 
 def calculate_vss_interchange() -> None:
     raise NotImplementedError
+
+
+calculate_baseii_interchange(
+    fs.Layer.STAGING, fs.Layer.STAGING, "DEMO", "CDA26F0BEB4349D03346A721DDCF0DC7"
+)
